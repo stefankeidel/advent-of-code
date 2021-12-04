beaa8a6fe65a9a53f2ef1ea3164190f454b029d5
Stefan Keidel <stefan.keidel@lichtblick.de>
2021-12-04 21:48:56 +0100

    Slightly more tolerable

--- 03/life_support.py
+++ 03/life_support.py
@@ -1,45 +1,33 @@
 from functools import reduce
+import operator
 
 
 def main():
     i = [list(x) for x in get_input()]
-
-    # we still need this, no?
-    z = reduce(bin_dim_sum, i)
-    gamma_rate = [0 if int(x) < len(i)/2 else 1 for x in z]
-
-    oxy = i
-
-    j = 0
-    while j < len(gamma_rate):
-        oxy = [x for x in oxy if int(x[j]) == gamma_rate[j]]
-        z = reduce(bin_dim_sum, oxy)
-        gamma_rate = [0 if int(x) < len(oxy)/2 else 1 for x in z]
-        if len(oxy) == 1:
-            break
-        j+=1
-
-    int_oxy = int("".join(str(i) for i in oxy[0]), 2)
-
-
-    # terrible copy+paste
-    z = reduce(bin_dim_sum, i)
-    gamma_rate = [1 if int(x) < len(i)/2 else 0 for x in z]
-
-    oxy = i
-
-    j = 0
-    while j < len(gamma_rate):
-        oxy = [x for x in oxy if int(x[j]) == gamma_rate[j]]
-        z = reduce(bin_dim_sum, oxy)
-        gamma_rate = [1 if int(x) < len(oxy)/2 else 0 for x in z]
-        if len(oxy) == 1:
-            break
-        j+=1
-
-    int_co2 = int("".join(str(i) for i in oxy[0]), 2)
+    int_oxy = compute_life_support(i, operator.lt)
+    int_co2 = compute_life_support(i, operator.ge)
 
     print(int_oxy * int_co2)
+
+
+def compute_life_support(input_list, comp_fn):
+    num_list = input_list
+
+    j = 0
+    while True:
+        z = reduce(bin_dim_sum, num_list)
+        gamma_rate = [0 if comp_fn(int(x), len(num_list) / 2) else 1 for x in z]
+        num_list = [x for x in num_list if int(x[j]) == gamma_rate[j]]
+
+        if len(num_list) == 1:
+            break
+
+        j += 1
+        if j == len(gamma_rate):
+            break
+
+    return int("".join(str(i) for i in num_list[0]), 2)
+
 
 
 def bin_dim_sum(x, y):
@@ -47,7 +35,7 @@ def bin_dim_sum(x, y):
 
 
 def get_input():
-    with open('input.txt') as f:
+    with open('test_input.txt') as f:
         return f.read().splitlines()
 
 if __name__ == "__main__":
