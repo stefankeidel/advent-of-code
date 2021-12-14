8043db9bd92cc8ee7d3ec190e128dedd3d41c03c
Stefan <1188614+stefankeidel@users.noreply.github.com>
2021-12-14 21:17:53 +0100

    13

--- 13/origami.py
+++ 13/origami.py
@@ -20,17 +20,39 @@ example_input = """6,10
 9,0
 """
 
-example_folds = """
-fold along y=7
+example_folds = """fold along y=7
 fold along x=5
 """
 
 def main():
-    dimx, dimy, dots, folds = read_input()
+    with open('dots.txt') as f:
+        with open('folds.txt') as z:
+            dimx, dimy, dots, folds = read_input(f.read(), z.read())
     ori = np.zeros((dimy+1, dimx+1), dtype=int)
 
     for (x, y) in dots:
         ori[y, x] = 1
+
+    for (dim, fold) in folds:
+        if dim == 'y':
+            ori_upper = ori[0:fold, 0:ori.shape[1]]
+            ori_lower = ori[fold+1:ori.shape[0], 0:ori.shape[1]]
+            ori_upper = np.flipud(ori_upper)
+
+            if ori_upper.shape[0] > ori_lower.shape[0]:
+                ori_lower = np.concatenate((ori_lower, np.zeros((1,ori_upper.shape[1]))))
+            ori = ori_lower + ori_upper
+            ori = np.flipud(ori)
+        elif dim == 'x':
+            ori_left = ori[0:ori.shape[0], 0:fold]
+            ori_right = ori[0:ori.shape[0], fold + 1:ori.shape[1]]
+
+            ori_left = np.fliplr(ori_left)
+            ori = ori_left + ori_right
+
+    nz = np.fliplr((ori > 0).astype(int))
+    print(np.count_nonzero(ori))
+    print(np.nonzero(ori))
 
     pass
 
@@ -50,9 +72,14 @@ def read_input(input=example_input, folds=example_folds):
 
         pairs.append((int(x), int(y)))
 
-    folds = []
+    fs = [x for x in folds.splitlines()]
+    ret_folds = []
+    for f in fs:
+        (fold_along, coord) = f.split('=')
 
-    return max_x, max_y, pairs, folds
+        ret_folds.append((fold_along[-1], int(coord)))
+
+    return max_x, max_y, pairs, ret_folds
 
 
 
